246705811f8aafdeca34b48dde3277deed29dd95
Joel George <129869710+Joelgeorgee@users.noreply.github.com>
2023-12-25 11:48:46 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,26 @@
+public import java.util.*;
+class Exception
+{
+	
+		void rec(int b)
+		{
+			if(b%2!=0)
+			{
+				throw new ArithmeticException();
+			}
+			else
+			{
+			System.out.println(b+" is an even number");
+			}
+		}
+}
+class demo
+{
+	public static void main(String args [])
+	{
+	Exception a = new Exception();
+	a.rec(33);
+	}
+
+} 
+
